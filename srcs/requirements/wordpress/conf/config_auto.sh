5e2c05ba41480a3731e83164abe1b0bf167bb7af
Waxxy <72367316@protonmail.com>
2023-03-03 15:06:15 +0100

    mariadb+nginx

--- srcs/requirements/wordpress/conf/config_auto.sh
+++ srcs/requirements/wordpress/conf/config_auto.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
 
-sleep 12
+sleep 10
 
 if [ ! -f '/var/www/wordpress/wp-config.php' ]; then
 	echo "[i] Wordpress is configuring...."
 	echo "	Linking database ${MYSQL_DATABASE} to  ${MYSQL_USER} with password ${MYSQL_PASSWORD}"
-	wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --path='/var/www/wordpress'
-	wp core install --title=$SITE_TITLE --admin_user=$ADMIN --admin_password=$ADMIN_PW --admin_email=$ADMIN_MAIL --skip-email --url=$WEBSITE --allow-root
-	wp user create $USER42 $USER42_MAIL --role=author --user_pass=$USER42_PW --allow-root
+	wp config create --allow-root \
+		--dbname=$MYSQL_DATABASE \
+		--dbuser=$MYSQL_USER \
+		--dbpass=$MYSQL_PASSWORD \
+		--dbhost=$MYSQL_HOST \
+		--path='/var/www/wordpress'
+	sleep 3
+	wp core install --title=$SITE_TITLE \
+		--admin_user=$ADMIN \
+		--admin_password=$ADMIN_PW \
+		--admin_email=$ADMIN_MAIL \
+		--skip-email --url=$WEBSITE \
+		--allow-root
+	wp user create $USER42 $USER42_MAIL \
+		--role=author \
+		--user_pass=$USER42_PW \
+		--allow-root \
+		--path='/var/www/wordpress' \
+		>> logs_wp.txt
 fi
 
 mkdir -p /usr/logs/php-fpm
