bb7004b5528b8549c6bad3cd53560516aeb67753
Waxxy <72367316@protonmail.com>
2023-03-08 18:51:47 +0100

    ftp with TLS

--- srcs/bonus/vsftpd/conf/run.sh
+++ srcs/bonus/vsftpd/conf/run.sh
@@ -5,9 +5,9 @@ if [ ! -f /etc/vsftpd/.my_ftp_config_ok ]; then
     echo "$FTP_USER:$FTP_PASSWORD" | /usr/sbin/chpasswd
     chown -R $FTP_USER:$FTP_USER /var/www/html
 	echo $FTP_USER >> /etc/vsftpd_userlist
-	echo "pasv_address=$(ip route | awk 'NR==1{print $3}')" >> /etc/vsftpd/vsftpd.conf
+	echo "pasv_address=127.0.0.1" >> /etc/vsftpd/vsftpd.conf
 	touch /etc/vsftpd/.my_ftp_config_ok
 fi
 
-echo "VSFTPD started, listen on port 21 on passive address $(ip route | awk 'NR==1{print $3}') and listening on address 0.0.0.0 and 127.0.0.1"
+echo "VSFTPD started, listen on port 21"
 exec /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
